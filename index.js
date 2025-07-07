f0dfc4a11dbd7265343587cba87aacb0978d9508
lolodev <horaciodev1706@gmail.com>
2025-07-07 16:24:37 -0300

    Render out emotions 2

--- index.js
+++ index.js
@@ -169,6 +169,9 @@ const catsData = [
     },
 ];
 
+const emotionRadios = document.querySelector("#emotion-radios");
+
+
 function getEmotionsArray(cats) {
     const emotionsArray = [];
     for (const cat of cats) {
@@ -180,8 +183,14 @@ function getEmotionsArray(cats) {
 }
 
 function renderEmotionsRadios(cats) {
+    let html = "";
     const emotions = getEmotionsArray(cats);
-    console.log(emotions);
+    for (const emotion of emotions) {
+        html += `
+        <p>${emotion}</p>
+        `;
+    }
+    emotionRadios.innerHTML = html;
 }
 
 renderEmotionsRadios(catsData);
